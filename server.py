6643ffff62df3e701e2e0668b5c376271098d5a5
takland1 <tak@landdigital.agency>
2024-10-23 14:05:25 +0100

    fix: creating RestfulAPI for chatbot and an index.html

--- server.py
+++ server.py
@@ -1,7 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify, make_response
+import uuid
+
+from chatgpt import Chatbot
+
+chatbot = Chatbot()
 
 app = Flask(__name__)
 
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
 @app.route("/dialogue/<query>")
 def dialogue(query):
-    return chatgpt.conversational_rag_chain()
+    if not query:
+        return "Please enter your query in a url like /dialogue/<query>"
+    result = chatbot.conversational_rag_chain().invoke({"input": query}, config={
+        "configurable": {"session_id": 'abc123'}
+    })
+    return result['answer']
+
+
+@app.route("/", methods=['POST', 'GET'])
+def query_view():
+    if request.method == 'POST':
+        print('step1')
+        session_id = request.cookies.get('session_id')
+        if not session_id:
+            return jsonify({'response': 'I need cookie to identify you.'})
+        query = request.form['prompt']
+        result = chatbot.conversational_rag_chain().invoke({"input": query}, config={
+            "configurable": {"session_id": session_id}
+        })
+        print('response', result)
+        return jsonify({'response': result['answer']})
+    response = make_response(render_template('index.html'))
+    session_id = uuid.uuid4()
+    response.set_cookie('session_id', str(session_id))
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
